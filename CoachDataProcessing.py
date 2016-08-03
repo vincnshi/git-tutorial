1442963c6aacce82bb022c90d71e006eb911e7d5
Vincent Shi <vincnshi@gmail.com>
2016-08-03 10:26:54 +0800

    Add CoachDataProcessing script by python

---
+++ CoachDataProcessing.py
@@ -0,0 +1,38 @@
+import os
+
+#from nester import print_lol
+
+import pickle
+
+os.chdir('C:\Personal_Data\Temp\Python_Practice\HeadFirstPython\data_processing')
+
+
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return(time_string)
+    (mins, secs) = time_string.split(splitter)
+    return (mins + '.' + secs)
+
+def get_coach_data(filename):
+    try:
+        with open(filename) as f:
+            data = f.readline
+        return(data.strip().split(','))
+    except IOError as ioerr:
+        print('File error:' + str(ioerr))
+        return(None)
+
+james = get_coach_data('james.txt')
+julie = get_coach_data('julie.txt')
+mikey = get_coach_data('mikey.txt')
+sarah = get_coach_data('sarah.txt')
+
+print(sorted(set(sanitize(each_t) for each_t in james))[0:3])
+print(sorted(set(sanitize(each_t) for each_t in julie))[0:3])
+print(sorted(set(sanitize(each_t) for each_t in mikey))[0:3])
+print(sorted(set(sanitize(each_t) for each_t in sarah))[0:3])
+
